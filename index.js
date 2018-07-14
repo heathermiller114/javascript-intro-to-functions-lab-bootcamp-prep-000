0f591307e6f980c8308ec76871d18afe48476956
Heather Miller <heather.miller114@gmail.com>
2018-07-14 03:33:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,8 @@ function sayHiToGrandma(string) {
   console.log(string.toLowerCase())
   return "I can't hear you!"
 }
+function sayHiToGrandma(string) {
+  console.log(string.toUpperCase())
+  return "YES INDEED!"
+}
+}
